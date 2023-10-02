379803ec55550b8efcc8ece63a16589aaaa2c558
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2023-10-01 21:36:46 -0400

    workshop 2 final version

--- data-service.js
+++ data-service.js
@@ -1,1 +1,65 @@
-// JavaScript source code
+const fs = require('fs').promises;
+const path = require('path');
+
+
+function initialize() {
+  return Promise.all([
+    fs.readFile('./data/vehicles.json', 'utf8')
+      .then((data) => JSON.parse(data))
+      .catch((err) => {
+        throw new Error(`Unable to read vehicles file}`);
+      }),
+    fs.readFile('./data/brands.json', 'utf8')
+      .then((data) => JSON.parse(data))
+      .catch((err) => {
+        throw new Error(`Unable to read brands file}`);
+      })
+  ])
+    .then(([vehicles, brands]) => {
+      return { vehicles, brands };
+    })
+    .catch((err) => {
+      throw new Error(`Data initialization fail}`);
+    });
+}
+
+
+function getAllVehicles() {
+  return initialize()
+    .then((data) => {
+      if (data.vehicles.length === 0) {
+        throw new Error('No results returned');
+      }
+      return data.vehicles;
+    });
+}
+
+
+function get2023Vehicles() {
+  return initialize()
+    .then((data) => {
+      const vehicles2023 = data.vehicles.filter((vehicle) => vehicle.year === 2023);
+      if (vehicles2023.length === 0) {
+        throw new Error('No results returned');
+      }
+      return vehicles2023;
+    });
+}
+
+
+function getBrands() {
+  return initialize()
+    .then((data) => {
+      if (data.brands.length === 0) {
+        throw new Error('No results returned');
+      }
+      return data.brands;
+    });
+}
+
+module.exports = {
+  getAllVehicles,
+  get2023Vehicles,
+  getBrands,
+  initialize,
+};
--- server.js
+++ server.js
@@ -1,1 +1,74 @@
-// JavaScript source code
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: _Frank Fu_____________________ Student ID: __126609197____________ Date: _Oct 1 2023_
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/ 
+const express = require('express'); 
+const dataService = require('./data-service');
+const app = express(); 
+const path = require('path');
+const HTTP_PORT = process.env.PORT || 8080; 
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+
+app.get('/vehicles', (req, res) => {
+    dataService.getAllVehicles()
+        .then((vehicles) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(vehicles);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+
+app.get('/vehicles2023', (req, res) => {
+    dataService.get2023Vehicles()
+        .then((vehicles2023) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(vehicles2023);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+
+app.get('/brands', (req, res) => {
+    dataService.getBrands()
+        .then((brands) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(brands);
+        })
+        .catch((err) => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found');
+});
+
+dataService.initialize()
+    .then(() => {
+        app.listen(HTTP_PORT, () => console.log(`Server listening on: ${HTTP_PORT}`));
+    })
+    .catch((err) => {
+        console.error(`Data initialization failed: ${err.message}`);
+    });
